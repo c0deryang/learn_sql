6ff465bc9969fea25acc0762fb1ac902bb77e94c
Docomo <yyang812@gmail.com>
2017-11-15 15:30:08 +0800

    '修改为终极版查询语句'

--- 2.sql
+++ 2.sql
@@ -29,6 +29,7 @@ CREATE TABLE source(
 	FOREIGN KEY (kcbh) REFERENCES course(kcbh)
 );
 
+/*
 ALTER TABLE students CHARACTER SET UTF8;
 ALTER TABLE course CHARACTER SET UTF8;
 ALTER TABLE source CHARACTER SET UTF8;
@@ -38,6 +39,7 @@ ALTER TABLE students CHANGE bj bj VARCHAR(50) CHARACTER SET UTF8;
 ALTER TABLE students CHANGE jg jg VARCHAR(50) CHARACTER SET UTF8;
 ALTER TABLE students CHANGE adds adds VARCHAR(50) CHARACTER SET UTF8;
 ALTER TABLE course CHANGE kcmc kcmc VARCHAR(50) CHARACTER SET UTF8;
+*/
 
 INSERT INTO students(no, name, birthday, bj, jg, adds, numbers) VALUES('1','张三','1992-6-1','一班','上海','上海市浦东新区','123123');
 INSERT INTO students(no, name, birthday, bj, jg, adds, numbers) VALUES('2','李四','1992-10-1','二班','河南','河南省洛阳市','456456');
@@ -58,3 +60,22 @@ INSERT INTO source(cjguid, no, kcbh, cj) VALUES(10007, '3', '10002', 80);
 INSERT INTO source(cjguid, no, kcbh, cj) VALUES(10008, '3', '10001', 66);
 INSERT INTO source(cjguid, no, kcbh, cj) VALUES(10009, '3', '10002', 75);
 INSERT INTO source(cjguid, no, kcbh, cj) VALUES(10010, '3', '10003', 95);
+
+
+#10.1.20.6  port 40022
+#fortestgroupmro66ty
+#yangyang
+
+#不完全版
+SELECT kcbh, MAX(cj) AS max, MIN(cj) AS min, AVG(cj) AS avg, SUM(cj) AS sum 
+FROM source WHERE no =
+(SELECT no FROM students WHERE name = '王华')
+GROUP BY kcbh;
+
+#终极版
+SELECT course.kcmc, MAX(cj) AS max, MIN(cj) AS min, ROUND(AVG(cj)) AS avg, SUM(cj) AS sum 
+FROM source
+INNER JOIN course
+ON source.kcbh=course.kcbh
+WHERE no=(SELECT no FROM students WHERE name = '王华')
+GROUP BY course.kcmc;
